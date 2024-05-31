6f5b64e0062e051231a327ddc029ca9bcf639d73
Davidmide02 <Davidmide07@gmail.com>
2024-05-31 11:30:39 +0100

    navbar mobile

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -20,21 +20,22 @@ import {
   MenuItem,
   Transition,
 } from "@headlessui/react";
+import Logo from "@/components/logo";
 //   import {Transition} from '@heroicons/react/20/solid'
 
 const navigation = [
   { name: "Sign In", href: "#", icon: HomeIcon, count: "5", current: true },
   { name: "Log out", href: "#", icon: UsersIcon, current: false },
   { name: "Setting", href: "#", icon: FolderIcon, count: "12", current: false },
-  {
-    name: "Calendar",
-    href: "#",
-    icon: CalendarIcon,
-    count: "20+",
-    current: false,
-  },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
+  // {
+  //   name: "Calendar",
+  //   href: "#",
+  //   icon: CalendarIcon,
+  //   count: "20+",
+  //   current: false,
+  // },
+  // { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
+  // { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
 ];
 const teams = [
   { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
@@ -46,151 +47,35 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Example({
+export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    // <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600">
-    //   <div className="mobile md:hidden flex justify-between items-center p-2">
-    //     <div className="logo flex h-16 shrink-0 items-center">
-    //       <h1 className="h-8 w-auto">Naija AI </h1>
-    //     </div>
-    //     <div className="h-6 w-6 shrink-0">
-    //       <HomeIcon />
-    //     </div>
-    //   </div>
-    //   {/*mobile end  */}
-    //   <div className="control bg-green-800 md:grid md:grid-cols-4">
-    //     <nav className="hidden md:flex flex-1 flex-col p-4 bg-yellow-400">
-    //       <div className=" logo flex h-16 shrink-0 items-center">
-    //         <h1 className="h-8 w-auto">Naija AI </h1>
-    //       </div>
-    //       <ul role="list" className="flex flex-1 flex-col gap-y-7">
-    //         <li>
-    //           <ul role="list" className="-mx-2 space-y-1">
-    //             {navigation.map((item) => (
-    //               <li key={item.name}>
-    //                 <a
-    //                   href={item.href}
-    //                   className={classNames(
-    //                     item.current
-    //                       ? "bg-indigo-700 text-white"
-    //                       : "text-indigo-200 hover:text-white hover:bg-indigo-700",
-    //                     "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-    //                   )}
-    //                 >
-    //                   <item.icon
-    //                     className={classNames(
-    //                       item.current
-    //                         ? "text-white"
-    //                         : "text-indigo-200 group-hover:text-white",
-    //                       "h-6 w-6 shrink-0"
-    //                     )}
-    //                     aria-hidden="true"
-    //                   />
-    //                   {item.name}
-    //                   {item.count ? (
-    //                     <span
-    //                       className="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-indigo-600 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-white ring-1 ring-inset ring-indigo-500"
-    //                       aria-hidden="true"
-    //                     >
-    //                       {item.count}
-    //                     </span>
-    //                   ) : null}
-    //                 </a>
-    //               </li>
-    //             ))}
-    //           </ul>
-    //         </li>
-    //         <li>
-    //           <div className="text-xs font-semibold leading-6 text-indigo-200">
-    //             Your teams
-    //           </div>
-    //           <ul role="list" className="-mx-2 mt-2 space-y-1">
-    //             {teams.map((team) => (
-    //               <li key={team.name}>
-    //                 <a
-    //                   href={team.href}
-    //                   className={classNames(
-    //                     team.current
-    //                       ? "bg-indigo-700 text-white"
-    //                       : "text-indigo-200 hover:text-white hover:bg-indigo-700",
-    //                     "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-    //                   )}
-    //                 >
-    //                   <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-    //                     {team.initial}
-    //                   </span>
-    //                   <span className="truncate">{team.name}</span>
-    //                 </a>
-    //               </li>
-    //             ))}
-    //           </ul>
-    //         </li>
-    //         <li className="-mx-6 mt-auto">
-    //           <a
-    //             href="#"
-    //             className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-indigo-700"
-    //           >
-    //             {/* <Image src={{"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"}} alt={'img'} width={40} height={20}/> */}
-    //             <span className="sr-only">Your profile</span>
-    //             <span aria-hidden="true">Tom Cook</span>
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //     <Disclosure as="nav" className="bg-gray-800">
-    //       {({ open }) => (
-    //         <>
-    //           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-    //             {/* Mobile menu button*/}
-    //             <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-    //               <span className="absolute -inset-0.5" />
-    //               <span className="sr-only">Open main menu</span>
-    //               {open ? (
-    //                 <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-    //               ) : (
-    //                 <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-    //               )}
-    //             </DisclosureButton>
-    //           </div>
-    //         </>
-    //       )}
-    //     </Disclosure>
-
-    //     <div className="content col-span-3">{children}</div>
-    //   </div>
-    // </div>
-    <div className="control m-auto md:grid md:grid-cols-3">
-      <div className="menu bg-green-300 p-4">
-        <div className="logo hidden">
-          <h1>Naija Ai</h1>
-        </div>
+    <div className="control m-auto md:grid md:grid-cols-3 text-white">
+      {/* mobile menu */}
+      <div className="menu p-4 bg-gray-800 md:hidden">
         <Disclosure as="nav" className="bg-gray-800">
           {({ open }) => (
             <>
-              <div className="div bg-green-200 flex justify-between items-center md:hidden">
-                {/* <div className="inset-y-0 left-0 flex items-center sm:hidden bg-green-600"> */}
+              <div className="div flex justify-between items-center">
                 {/* Mobile menu button*/}
-                <div className="logo">
-                  <h1>Naija Ai</h1>
-                </div>
-                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Logo />
+                <DisclosureButton className="relative text-white inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className="block h-8 w-8 " aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className="block h-8 w-8" aria-hidden="true" />
                   )}
                 </DisclosureButton>
               </div>
-              <DisclosurePanel className="md:hidden absolute left-0 top-0 h-screen bg-gray-800">
-                <div className="bg-purple-600 h-full py-2">
-                  <div className="new-chat bg-yellow-300 h-[70%] p-2">
-                    <h3 className="">New chat here</h3>
+              <DisclosurePanel className="absolute left-0 top-0 h-screen w-[80%] bg-gray-800">
+                <div className="h-full py-2">
+                  <div className="new-chat h-[70%] p-2">
+                    <h3 className="text-center">New chat here</h3>
                   </div>
 
                   <div className="space-y-1 px-2 pb-3 pt-2">
@@ -203,7 +88,7 @@ export default function Example({
                           item.current
                             ? "bg-gray-900 text-white"
                             : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "block rounded-md px-3 py-2 text-base font-medium"
+                          "block rounded-md px-3 py-2 text-base font-medium text-center"
                         )}
                         aria-current={item.current ? "page" : undefined}
                       >
@@ -217,8 +102,13 @@ export default function Example({
           )}
         </Disclosure>
       </div>
+      <div className="desktop hidden md:block bg-green-400">
 
-      <div className="children bg-red-600 col-span-2 p-4">{children}</div>
+
+      </div>
+
+      {/* children */}
+      <div className="children bg-gray-600 col-span-2 p-4">{children}</div>
     </div>
   );
 }
@@ -294,13 +184,14 @@ export default function Example({
   /* </div> */
 }
 
-<Disclosure as="nav" className="bg-gray-800 flex flex-col h-screen">
+{
+  /* <Disclosure as="nav" className="bg-gray-800 flex flex-col h-screen">
   {({ open }) => (
     <>
       <div className="my-auto max-h-7xl px-2 sm:py-6 lg:py-8">
         <div className="relative flex items-center justify-between bg-yellow-500">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden bg-green-600">
-            {/* Mobile menu button*/}
+           
             <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
@@ -316,7 +207,6 @@ export default function Example({
             <div className="flex flex-shrink-0 items-center">Logo</div>
 
             <div className="hidden sm:ml-6 sm:block">
-              {/* <div className="hidden sm:ml-6 sm:block"> */}
               <div className="flex flex-col space-x-4">
                 {navigation.map((item) => (
                   <a
@@ -361,4 +251,5 @@ export default function Example({
       </DisclosurePanel>
     </>
   )}
-</Disclosure>;
+</Disclosure>; */
+}
---
+++ src/components/logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className="logo text-large">
+    <h1>Naija Ai</h1>
+  </div>
+  )
+}
+
+export default Logo
